352c8b90cc0d50f13e8b2e51589681b03192d420
nehapprakash <50229821+nehapprakash@users.noreply.github.com>
2020-11-17 20:11:04 +0530

    Add files via upload

---
+++ adminlog/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2019 Course Enrollment portal | By : <a href="http://www.cittumkur.com/" target="_blank">CIT</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
